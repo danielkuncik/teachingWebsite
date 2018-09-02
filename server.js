d1132911a91f62f0460bb931e89721b4bc5c988d
Daniel Kuncik <dkuncik@gmail.com>
2018-09-02 10:29:08 -0400

    Setup Startscript and Heroku Port

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
+
 app.get('/', (req, res) => {
     res.send('Welcome to Physics!')
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is running on port $[port]`);
+});
