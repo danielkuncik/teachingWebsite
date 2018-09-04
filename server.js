f97ad845537cbe4dfb96cccac1ab5537ca5052a1
Daniel Kuncik <dkuncik@gmail.com>
2018-09-04 07:58:07 -0400

    set up initial page

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Physics!')
+    res.send("Welcome to Physics! <a href = '/basicPage.html'>Click Here<a>")
 });
 
 app.listen(port, () => {
